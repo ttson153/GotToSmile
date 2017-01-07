192f5527d6f263423b6341c49f849f92c1be15c0
beekill <nnn_bikiu0707@yahoo.com>
2017-01-07 17:50:35 +0700

    Partially implement search for poster

--- app/src/main/java/com/google/android/gms/samples/vision/face/facetracker/ui/activity/MainActivity.java
+++ app/src/main/java/com/google/android/gms/samples/vision/face/facetracker/ui/activity/MainActivity.java
@@ -1,12 +1,16 @@
 package com.google.android.gms.samples.vision.face.facetracker.ui.activity;
 
 import android.content.Intent;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.EditText;
 import android.widget.GridView;
 
 import com.google.android.gms.samples.vision.face.facetracker.Poster;
@@ -48,6 +52,26 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.search_menu, menu);
+
+        // add callback for text changed
+        EditText searchEditText = (EditText) MenuItemCompat.getActionView(menu.findItem(R.id.searchEditText));
+        searchEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                // do nothing
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                // do nothing
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                String searchString = s.toString();
+                onSearchPoster(searchString);
+            }
+        });
         return true;
     }
 
@@ -55,5 +79,10 @@ public class MainActivity extends AppCompatActivity {
         Intent showFullPosterIntent = new Intent(MainActivity.this, PosterFullView.class);
         showFullPosterIntent.putExtra("Full_view", poster);
         startActivity(showFullPosterIntent);
+    }
+
+    private void onSearchPoster(String filmName) {
+        // TODO: search for film name
+        // and display
     }
 }
